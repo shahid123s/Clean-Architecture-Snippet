ad3d7e5ec5e51c8da52113478753e60c3fa76728
shahid123s <shahidnoushad13s@gmail.com>
2025-11-27 03:50:11 +0530

    chore: remove clean architecture user management example, update README, and add CHANGELOG.

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  port: process.env.PORT || 3000,
-  env: process.env.NODE_ENV || 'development',
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/clean-arch-db',
-};
